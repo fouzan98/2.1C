322190ad6fbbaf58865b6371d92665aa48f2c137
fouzan98 <142594751+fouzan98@users.noreply.github.com>
2023-09-03 21:54:37 +0530

    PART 1

---
+++ script.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        input: '',
+        items: [
+            { name: '50$ ' },
+            { name: '100$' },
+            { name: '150$' },
+            { name: '200$' }
+        ]
+    }
+  });
